3f1cd6e3015e3afd875dacf0c5165ca4ef88dd83
Edy Segura <edysegura@gmail.com>
2016-06-10 20:12:43 -0300

    Added FactorialService.js

---
+++ app/js/FactorialService.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var FactorialService = (function (params) {
+
+    function calculate() {
+        return null;
+    }
+
+    //public api
+    return {
+        "calculate": calculate
+    }
+
+})();

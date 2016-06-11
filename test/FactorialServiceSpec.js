25f9d7290a4d753b8fdfaea3175738f36f427ddb
Edy Segura <edysegura@gmail.com>
2016-06-10 21:27:00 -0300

    Increase the test cases

--- test/FactorialServiceSpec.js
+++ test/FactorialServiceSpec.js
@@ -3,6 +3,11 @@
 describe('FactorialService.js', function() {
     
     it('should calculate factorial from a given name', function() {
+        expect(FactorialService.calculate(0)).toBe(1);
+        expect(FactorialService.calculate(1)).toBe(1);
+        expect(FactorialService.calculate(2)).toBe(2);
+        expect(FactorialService.calculate(3)).toBe(6);
+        expect(FactorialService.calculate(4)).toBe(24);
         expect(FactorialService.calculate(5)).toBe(120);
     });
         

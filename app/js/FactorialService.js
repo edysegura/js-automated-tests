ee95bbe6c834fc2f2735e9a64aeb9a1e06c0c80a
Edy Segura <edysegura@gmail.com>
2016-06-11 00:09:23 -0300

    Making the test pass

--- app/js/FactorialService.js
+++ app/js/FactorialService.js
@@ -2,15 +2,24 @@
 
 var FactorialService = (function () {
 
-    function calculate(number) {
-        var result = 1;
-        if(number === 0 || number === 1) return result;
-        
-        for (var i = 1; i <= number; i++) {
-            result = result * i;
-        }
+    function _isValidNumber(number) {
+        return (number !== undefined)
+            && (typeof(number) !== 'string')  
+            && (number > -1);
+    }
 
-        return result;
+    function calculate(number) {
+        if (_isValidNumber(number)) {
+            var result = 1;
+            if (number === 0 || number === 1) return result;
+
+            for (var i = 1; i <= number; i++) {
+                result = result * i;
+            }
+
+            return result;
+        }
+        return null;
     }
 
     //public api

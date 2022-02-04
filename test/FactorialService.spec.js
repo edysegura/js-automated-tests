7750d70006dcfc0280fb9c5ef36bd5377d504350
Edy Segura <edysegura@gmail.com>
2022-02-03 22:27:06 -0300

    chore: prettierfy factorial service

--- test/FactorialService.spec.js
+++ test/FactorialService.spec.js
@@ -1,7 +1,7 @@
 import { FactorialService } from '../app/js/FactorialService'
 
-describe('FactorialService.js', function() {
-  it('should calculate factorial from a given number', function() {
+describe('FactorialService.js', function () {
+  it('should calculate factorial from a given number', function () {
     expect(FactorialService.calculate(1)).toBe(1)
     expect(FactorialService.calculate(2)).toBe(2)
     expect(FactorialService.calculate(3)).toBe(6)
@@ -9,11 +9,11 @@ describe('FactorialService.js', function() {
     expect(FactorialService.calculate(5)).toBe(120)
   })
 
-  it('should return 1 (one) if the given number is 0 (zero)', function() {
+  it('should return 1 (one) if the given number is 0 (zero)', function () {
     expect(FactorialService.calculate(0)).toBe(1)
   })
 
-  it('should return undefined for given invalid params', function() {
+  it('should return undefined for given invalid params', function () {
     expect(FactorialService.calculate()).toBe(undefined)
     expect(FactorialService.calculate(-1)).toBe(undefined)
     expect(FactorialService.calculate('')).toBe(undefined)

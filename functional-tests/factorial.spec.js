5639a769d4dcafe631dad5fce3a4e3a5e26a3bf3
Edy Segura <edysegura@gmail.com>
2025-02-20 22:12:53 -0300

    test: update functional tests for Factorial Calculator and comment out unused tests

--- functional-tests/factorial.spec.js
+++ functional-tests/factorial.spec.js
@@ -1,19 +1,23 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
+
+const baseURL = 'http://localhost:3000'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(baseURL)
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle(/Factorial Calculator/)
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto(baseURL)
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click()
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole('heading', { name: 'Installation' })
+//   ).toBeVisible()
+// })

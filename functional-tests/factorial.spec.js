993be0888702374352f701787fc7d2ee114d8b1d
Edy Segura <edysegura@gmail.com>
2025-02-21 17:14:55 -0300

    chore(test): enhance functional tests for Factorial Calculator and add factorial calculation test

--- functional-tests/factorial.spec.js
+++ functional-tests/factorial.spec.js
@@ -5,19 +5,12 @@ const baseURL = process.env.BASE_URL || 'http://localhost:3000'
 
 test('has title', async ({ page }) => {
   await page.goto(baseURL)
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Factorial Calculator/)
+  await expect(page).toHaveTitle('Factorial Calculator')
 })
 
-// test('get started link', async ({ page }) => {
-//   await page.goto(baseURL)
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click()
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole('heading', { name: 'Installation' })
-//   ).toBeVisible()
-// })
+test('calculates factorial of 5', async ({ page }) => {
+  await page.goto(baseURL)
+  await page.getByLabel('Number').fill('5')
+  await page.getByRole('button', { name: 'Calculate' }).click()
+  await expect(page.getByText('120')).toBeVisible()
+})
